a21e6986a5d2a76aee29a905c477f70ebe023e57
simonbf15 <barralesimon@gmail.com>
2023-06-03 19:07:05 -0300

    pequeños cambios / identar código / documentación funciones Js

--- Preguntados/funciones.js
+++ Preguntados/funciones.js
@@ -1,12 +1,10 @@
 /**
- * Descripción: Función que al pasar el mouse por el canvas muestra un mensaje.
- * @method - textoCanvas
- * @return - no retorna ningún valor.
+ * Descripción Función que al pasar el mouse por el canvas muestra un mensaje.
+ * @method  textoCanvas
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @param {number} ParámetroB - Explicación de que valor almacena ParámetroB
+ * @return  no retorna ningún valor.
  */
-
-
-
-
 let textoCanvas = () => {
     const canvas = document.getElementById("canvas1");
     const ctx = canvas.getContext('2d');
@@ -16,14 +14,12 @@ let textoCanvas = () => {
 }
 
 
-
-
 /**
- * Descripción - La función comprueba si ingresaste algún valor al input
- * @method - validarFormulario
- * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * Descripción  La función comprueba si ingresaste algún valor al input
+ * @method  validarFormulario
+ * @param {string} ParámetroA - almacena un evento que se activa al cargarse la página, luego va a activarse otro evento cuando la página envíe el formulario.
  * @param {number} ParámetroB - Explicación de que valor almacena ParámetroB
- * @return - Valor que retorna
+ * @return si se valida el formulario, se envia el valor del input
  */
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -42,14 +38,14 @@ function validarFormulario(evento) {
 
 
 /**
- * Descripción - La función cuenta la cantidad de respuestas correctas
- * @method - cantidadCorrectas
+ * La función cuenta la cantidad de respuestas correctas (hay 4 funciones, 1 para cada tema)
+ * @method cantidadCorrectas
  * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
  * @param {number} ParámetroB - Explicación de que valor almacena ParámetroB
- * @return - Valor que retorna
+ * @return returna let cantidad
  */
 
-let  cantidadCorrectas  = () => {
+let cantidadCorrectas = () => {
     let p1, p2, p3, p4, p5, cantidad;
 
     if (document.getElementById('preg1_2').checked == true) {
@@ -66,23 +62,33 @@ let  cantidadCorrectas  = () => {
         p3 = 1;
     } else {
         p3 = 0;
-    }if (document.getElementById('preg4_3').checked == true) {
+    }
+    if (document.getElementById('preg4_3').checked == true) {
         p4 = 1;
     } else {
         p4 = 0;
-    }if (document.getElementById('preg5_3').checked == true) {
+    }
+    if (document.getElementById('preg5_3').checked == true) {
         p5 = 1;
     } else {
         p5 = 0;
     }
 
 
-cantidad=p1+p2+p3+p4+p5;
-alert('cantidad de aciertos: '+cantidad);
-window.location = 'hist_mundial.html';
+    cantidad = p1 + p2 + p3 + p4 + p5;
+    alert('cantidad de aciertos: ' + cantidad);
+    window.location = 'hist_mundial.html';
 }
 
-let  cantidadCorrectas2  = () => {
+/**
+ * La función cuenta la cantidad de respuestas correctas (hay 4 funciones, 1 para cada tema)
+ * @method cantidadCorrectas2
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @param {number} ParámetroB - Explicación de que valor almacena ParámetroB
+ * @return returna let cantidad
+ */
+
+let cantidadCorrectas2 = () => {
     let p1, p2, p3, p4, p5, cantidad;
 
     if (document.getElementById('preg1_1').checked == true) {
@@ -99,22 +105,32 @@ let  cantidadCorrectas2  = () => {
         p3 = 1;
     } else {
         p3 = 0;
-    }if (document.getElementById('preg4_3').checked == true) {
+    }
+    if (document.getElementById('preg4_3').checked == true) {
         p4 = 1;
     } else {
         p4 = 0;
-    }if (document.getElementById('preg5_4').checked == true) {
+    }
+    if (document.getElementById('preg5_4').checked == true) {
         p5 = 1;
     } else {
         p5 = 0;
     }
 
 
-    cantidad=p1+p2+p3+p4+p5;
-    alert('cantidad de aciertos: '+cantidad);
+    cantidad = p1 + p2 + p3 + p4 + p5;
+    alert('cantidad de aciertos: ' + cantidad);
     window.location = 'mexico_86.html';
 }
-let  cantidadCorrectas3  = () => {
+
+/**
+ * La función cuenta la cantidad de respuestas correctas (hay 4 funciones, 1 para cada tema)
+ * @method cantidadCorrectas3
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @param {number} ParámetroB - Explicación de que valor almacena ParámetroB
+ * @return returna let cantidad
+ */
+let cantidadCorrectas3 = () => {
     let p1, p2, p3, p4, p5, cantidad;
 
     if (document.getElementById('preg1_2').checked == true) {
@@ -131,23 +147,32 @@ let  cantidadCorrectas3  = () => {
         p3 = 1;
     } else {
         p3 = 0;
-    }if (document.getElementById('preg4_4').checked == true) {
+    }
+    if (document.getElementById('preg4_4').checked == true) {
         p4 = 1;
     } else {
         p4 = 0;
-    }if (document.getElementById('preg5_4').checked == true) {
+    }
+    if (document.getElementById('preg5_4').checked == true) {
         p5 = 1;
     } else {
         p5 = 0;
     }
 
 
-    cantidad=p1+p2+p3+p4+p5;
-    alert('cantidad de aciertos: '+cantidad);
+    cantidad = p1 + p2 + p3 + p4 + p5;
+    alert('cantidad de aciertos: ' + cantidad);
     window.location = 'uruguay_30.html';
 }
 
-let  cantidadCorrectas4  = () => {
+/**
+ * La función cuenta la cantidad de respuestas correctas (hay 4 funciones, 1 para cada tema)
+ * @method cantidadCorrectas4
+ * @param {string} ParámetroA - Explicación de que valor almacena ParámetroA
+ * @param {number} ParámetroB - Explicación de que valor almacena ParámetroB
+ * @return returna let cantidad
+ */
+let cantidadCorrectas4 = () => {
     let p1, p2, p3, p4, p5, cantidad;
 
     if (document.getElementById('preg1_4').checked == true) {
@@ -164,18 +189,20 @@ let  cantidadCorrectas4  = () => {
         p3 = 1;
     } else {
         p3 = 0;
-    }if (document.getElementById('preg4_3').checked == true) {
+    }
+    if (document.getElementById('preg4_3').checked == true) {
         p4 = 1;
     } else {
         p4 = 0;
-    }if (document.getElementById('preg5_2').checked == true) {
+    }
+    if (document.getElementById('preg5_2').checked == true) {
         p5 = 1;
     } else {
         p5 = 0;
     }
 
 
-    cantidad=p1+p2+p3+p4+p5;
-    alert('cantidad de aciertos: '+cantidad);
+    cantidad = p1 + p2 + p3 + p4 + p5;
+    alert('cantidad de aciertos: ' + cantidad);
     window.location = 'qatar_22.html';
 }
