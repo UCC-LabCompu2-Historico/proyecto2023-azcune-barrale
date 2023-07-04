540bd6cc8ce80562aa3a599ff3d6716da03c958c
Axel <axelazcune@hotmail.com.ar>
2023-07-04 13:22:30 -0300

    Canvas

--- Preguntados/funciones.js
+++ Preguntados/funciones.js
@@ -1,16 +1,42 @@
 /**
- * Descripción Función que al pasar el mouse por el canvas muestra un mensaje.
- * @method  textoCanvas
-
- * @return  no retorna ningún valor.
+ * La función cambia la imagen del canvas al azar
+ * @method cambiarImagenAlAzar
+ * @return return la imagen
  */
-let textoCanvas = () => {
-    const canvas = document.getElementById("canvas1");
-    const ctx = canvas.getContext('2d');
 
-    ctx.font = "18px Arial"
-    ctx.fillText("Primero: suerte en el juego, segundo Francia.", 20, 100);
+function cambiarImagenAlAzar() {
+    const canvas = document.getElementById("canvas1");
+    const context = canvas.getContext("2d");
+
+    const imagenes = ["../imagenes/imagen1.jpg", "../imagenes/imagen2.jpg", "../imagenes/imagen3.jpg", "../imagenes/imagen4.jpg", "../imagenes/imagen5.jpg", "../imagenes/imagen6.jpg", "../imagenes/imagen7.jpg", "../imagenes/imagen8.jpg", "../imagenes/imagen9.jpg", "../imagenes/imagen10.jpg", "../imagenes/imagen11.jpg"];
+
+    const indice = Math.floor(Math.random() * imagenes.length);
+
+    const imagen = new Image();
+    imagen.src = imagenes[indice];
+
+    imagen.onload = function() {
+        //para calcular la escala, se divide el ancho del canvas por el ancho de la imagen
+        const escala = Math.max(canvas.width / imagen.width, canvas.height / imagen.height);
+        const ancho = imagen.width * escala;
+        const alto = imagen.height * escala;
+
+        //se calcula la diferencia entre el ancho del canvas y el ancho de la imagen
+        const x = (canvas.width - ancho) / 2;
+        const y = (canvas.height - alto) / 2;
+
+        //vaciar
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        //dibuja la imagen en el canvas con la escala y el tamaño
+        context.drawImage(imagen, x, y, ancho, alto);
+    };
 }
+
+const canvas = document.getElementById("canvas1");
+canvas.addEventListener("click", cambiarImagenAlAzar);
+
+
 
 /**
  * La función cuenta la cantidad de respuestas correctas (hay 4 funciones, 1 para cada tema)
